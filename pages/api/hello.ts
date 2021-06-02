0bd38a5ab60d1866f31d3f91527c1fb35c2d1b42
Zach Green <zhgreen@gmail.com>
2021-06-02 18:44:57 +0200

    fix: remove debug

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("yo");
   res.status(200).json({ name: "John Doe" });
 };
